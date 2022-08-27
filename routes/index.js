e915e7db13b0bd959d6ccfdb6b4986b6e0c0d664
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-08-27 20:50:08 +0900

    update index.js 라우터추가

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const router = express.Router();
 
 const userRoter = require("./user.router");
 const roomRouter = require("./room.router");
-
+const mailRouter = require("../template/auth");
 
 router.use("/user", userRoter);
 router.use("/room", roomRouter);
-
+router.use("/mail", mailRouter);
 
 module.exports = router;
 
