7c21b02e11a1d88a74d0e4c473b976b56679f551
cesdea <aprkvnem@gmail.com>
2022-08-27 17:35:40 +0900

    test socket  update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { sequelize } = require("./models");
-// const indexRouter = require("./routes");
+const indexRouter = require("./routes");
 const { error404, error } = require("./middlewares/error");
 const cors = require("cors");
+const IO=require("./socket/io")
 
 class App {
   constructor() {
     this.app = express();
     this.setMiddleWare();
     this.setRouter();
+    this.setIo(app);
     // this.setErrorHandler();
   }
   setMiddleWare() {
@@ -17,7 +19,15 @@ class App {
     this.app.use(cors());
   }
   setRouter() {
-    // this.app.use("/api", indexRouter);
+    this.app.use("/api", indexRouter);
+    this.app.get('/', function(req, res) {
+      res.sendFile(__dirname + '/static/index.html');
+    });
+  }
+  setIo=(app)=>{
+    this.server = http.createServer(app);
+    let io=new IO(server);
+    io.on('connection', );
   }
   setErrorHandler() {
     this.app.use(error404);
