36c0d28f9a8c065e5e5cf63d0178e106dae8285d
cesdea <aprkvnem@gmail.com>
2022-08-29 22:08:49 +0900

    update io, roomId검사

--- socket/io.js
+++ socket/io.js
@@ -8,73 +8,101 @@ class IO {
   constructor(app) {
     this.server = http.createServer(app);
     this.io = new Server(this.server);
-    // this.roomList = this.getRoomList;
-    this.roomList =[{roomId:1},{roomId:2}]
     this.connectServer();
   }
-  getRoomList = async () => {
-    const list = await this.roomRepository.getRoomList();
+  getRoomList = () => {
+    const list = this.roomRepository.getRoomList();
     return list;
   };
+
+  getRoomId=(room)=>{
+    const roomId = this.roomRepository.findRoomId(room);
+    return roomId;
+  }
+
   connectServer = async () => {
-    for (let i = 0; i < this.roomList.length; i++) {
-      const room=this.roomList[i].roomId;
-      let nsp = this.io.of("/"+room).on("connection", (socket) => {
-        // 접속한 클라이언트의 정보가 수신되면
-        socket.on("login", (data) => {
-          console.log(data)
-          console.log(
-            "Client logged-in:\n name:" +
-              data.name +
-              "\n userid: " +
-              data.userid +
-              "\n this room: " +
-              data.room
-          );
+    let roomList=await this.getRoomList()
+    console.log(roomList)
+    // let roomList =[{roomId:1},{roomId:2}]
+    let count = 0;
+    let nsp = this.io.on("connection", (socket) => {
+      // console.log(socket)
+      // 접속한 클라이언트의 정보가 수신되면
+      socket.on("login", async(data) => {
+        //user 검사
+        const user=data.userId
 
-          // socket에 클라이언트 정보를 저장한다
-          socket.name = data.name;
-          socket.userid = data.userid;
-
-          // 접속된 모든 클라이언트에게 메시지를 전송한다
-          nsp.emit("login", data.name);
-        });
-
-        // 클라이언트로부터의 메시지가 수신되면
-        socket.on("client_msg", (data) => {
-          var msg = {
-            from: {
-              name: socket.name,
-              userid: socket.userid,
-            },
-            msg: data.msg,
-          };
-
-          // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-          if (msg.msg !== "") {
-            console.log("IN %s,Message from %s: %s",data.room, socket.name, data.msg);
-            nsp.emit("server_msg", msg);
+        //room 검사
+        const room=data.room
+        if(roomList.filter((ele)=>(ele.roomId==room).length===0)){
+          let currentID=await this.getRoomId(room);
+          if(currentID===null || currentID===undefined ){
+            console.log("room: "+room+" is WRONG_URL");
+            socket.disconnect();
+            return;
+          }else{
+            roomList.push({roomId:room})
           }
+        } 
+        count++;
+        console.log(
+          "Client logged-in:\n nickname:" +
+            data.nickname +
+            "\n userid: " +
+            data.userid +
+            "\n this room: " +
+            data.room
+        );
+        // socket에 클라이언트 정보를 저장한다
+        socket.nickname = data.nickname;
+        socket.userid = data.userid;
+        socket.room=data.room;
 
-          // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
-          // socket.emit('s2c chat', msg);
-
-          // 접속된 모든 클라이언트에게 메시지를 전송한다
-          // io.emit('s2c chat', msg);
-
-          // 특정 클라이언트에게만 메시지를 전송한다
-          // io.to(id).emit('s2c chat', data);
-        });
-        // force client disconnect from server
-        socket.on("forceDisconnect", () => {
-          socket.disconnect();
-        });
-
-        socket.on("disconnect", () => {
-          console.log("user disconnected: " + socket.name);
-        });
+        socket.join(room);
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        nsp.to(data.room).emit("login", data.nickname);
       });
-    }
+
+      // 클라이언트로부터의 메시지가 수신되면
+      socket.on("client_msg", (data) => {
+        var msg = {
+          from: {
+            nickname: socket.nickname,
+            userid: socket.userid,
+          },
+          msg: data.msg,
+        };
+
+        // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
+        if (msg.msg !== "") {
+          console.log(
+            "IN %s,Message from %s: %s",
+            socket.room,
+            socket.nickname,
+            data.msg
+          );
+          nsp.to(socket.room).emit("server_msg", msg);
+        }
+
+        // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
+        // socket.emit('s2c chat', msg);
+
+        // 접속된 모든 클라이언트에게 메시지를 전송한다
+        // io.emit('s2c chat', msg);
+
+        // 특정 클라이언트에게만 메시지를 전송한다
+        // io.to(id).emit('s2c chat', data);
+      });
+      // force client disconnect from server
+      socket.on("forceDisconnect", () => {
+        count--;
+        socket.disconnect();
+      });
+
+      socket.on("disconnect", () => {
+        console.log("user disconnected: " + socket.name);
+      });
+    });
   };
   setErrorHandler() {
     this.io.use(error404);
