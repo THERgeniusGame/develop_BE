6af4ff636887e81022eaa4b51e4477289e2b760d
cesdea <aprkvnem@gmail.com>
2022-08-30 22:02:47 +0900

    update room validation funtion

--- socket/event/connection.js
+++ socket/event/connection.js
@@ -1,19 +1,20 @@
 const Login=require("./login.evnet")
 const Chat=require('./chat.event');
 const Disconnect=require("./disconnect.event")
+const RoomRepository = require("../repositories/room.repository");
 
-
+const roomRepository = new RoomRepository();
 getRoomList = () => {
-    const list = this.roomRepository.getRoomList();
+    const list = roomRepository.getRoomList();
     return list;
 };
 
 getRoomId=(room)=>{
-    const roomId = this.roomRepository.findRoomId(room);
+    const roomId = roomRepository.findRoomId(room);
     return roomId;
 }
 module.exports=(io)=>{
-    let nsp = io.on("connection", (socket) => {
+    io.on("connection", (socket) => {
         // console.log(socket)
         // 접속한 클라이언트의 정보가 수신되면
         Login(io,socket)
@@ -23,5 +24,4 @@ module.exports=(io)=>{
         // force client disconnect from server
         Disconnect(io,socket)
     })
-
 }
