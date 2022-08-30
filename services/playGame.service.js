8e421b9cd0a79de8932ac7c0b5b3d26921ef24de
cesdea <aprkvnem@gmail.com>
2022-08-30 21:09:14 +0900

    delete test code , api:api/room/:roomId

--- services/playGame.service.js
+++ services/playGame.service.js
@@ -1,1 +1,10 @@
-const router = express.require('express');
+const Repo=require("../repositories/room.repository")
+class Service{
+    repo=new Repo();
+    getRoomInfo=async(roomId)=>{
+        const getRoom=await this.repo.findRoomId(roomId);
+        return getRoom;
+    }
+}
+
+module.exports=Service
