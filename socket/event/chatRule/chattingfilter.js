770643e561ced5c662f9cf4e0d34db0299dddf34
cesdea <aprkvnem@gmail.com>
2022-09-02 21:12:33 +0900

    delete testCode, chattingfilter.js

--- socket/event/chatRule/chattingfilter.js
+++ socket/event/chatRule/chattingfilter.js
@@ -2,17 +2,7 @@
 const fs = require("fs");
 const path=require("path");
 const filterList = fs.readFileSync(path.resolve(__dirname, "./forbidwords.txt")).toString().split("\r\n");
-for (i in filterList) {
-}
-let message = [
-  //"내가 이겼어!!!!",
-  //"시발 존나 짜증나게 하지 마라",
-  //"안녕하세요",
-  //"개새끼입니까?",
-  "이 또라이같은 새끼야",
-  //"fuck you",
-  //"fuck",
-];
+
 module.exports=function(message) {
   //금기어가 포함된 Msg
   let badword = [];
