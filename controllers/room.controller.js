fa8df9b001a1f79bf468f0e0635c2e95dbd40f26
Jiwonemil <jjiwon24@gmail.com>
2022-09-05 11:10:14 +0900

    update try-catch 문으로 수정

--- controllers/room.controller.js
+++ controllers/room.controller.js
@@ -5,44 +5,49 @@ module.exports = class RoomController {
 
   //로비화면
   getRobby = async (req, res, next) => {
-    let pageNum = req.query.page;
-    let offset = 0;
+    try {
+      let pageNum = req.query.page;
+      let offset = 0;
 
-    if (pageNum > 1) {
-      offset = 6 * (pageNum - 1);
+      if (pageNum > 1) {
+        offset = 6 * (pageNum - 1);
+      }
+      const roomsInfo = await this.roomService.getRoomsInfo(offset);
+
+      return res.json({ roomsInfo });
+    } catch (err) {
+      next(err); //미들웨어로 보내는거
     }
-    const roomsInfo = await this.roomService.getRoomsInfo(offset);
-
-    return res.json({ roomsInfo });
   };
 
   //방만들기
   createRoom = async (req, res, next) => {
-    const { userId } = res.locals;
-    //const userId = 1; //test용
-    const { roomTitle, roomCategory, roomLock, roomPw } = req.body;
+    try {
+      //const { userId } = res.locals;
+      const userId = 1; //test용
+      const { roomTitle, roomCategory, roomLock, roomPw } = req.body;
 
-    if (!roomTitle) {
-      return res.status(400).send({ success: false, msg: "Invalid-Datatype" });
-    }
+      if (!roomTitle) {
+        let err = new Error("Invalid-Datatype");
+        err.status = 400;
+        throw err;
+      }
 
-    const createRoom = await this.roomService.createRoom(
-      roomTitle,
-      roomCategory,
-      roomLock,
-      roomPw,
-      userId
-    );
-
-    if (createRoom.success === true) {
-      res
-        .status(201)
-        .json({ success: true, data: { roomId: createRoom.result } });
-    } else {
-      return res.status(createRoom.status).send({
-        success: false,
-        msg: createRoom.msg,
-      });
+      const createRoom = await this.roomService.createRoom(
+        roomTitle,
+        roomCategory,
+        roomLock,
+        roomPw,
+        userId
+      );
+      if (createRoom.success === true) {
+        res
+          .status(201)
+          .json({ success: true, data: { roomId: createRoom.result } });
+      } else throw new Error();
+    } catch (err) {
+      err.status, err.massage;
+      next(err);
     }
   };
 };
