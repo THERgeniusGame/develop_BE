6aa57eebc6d8bfc4d154ab6b555c01d380cd67e7
cesdea <aprkvnem@gmail.com>
2022-09-06 15:22:32 +0900

    update game.evnet.js , 이름변경 위치변경

--- socket/event/game.event.js
+++ socket/event/game.event.js
@@ -2,20 +2,20 @@ const { error } = require("../middlewares/error");
 const GameService=require("../../services/playGame.service.js")
 let gameService=new GameService()
 class Game{
-    GameStart= async(io, socket,roomList) => {
-        socket.on("gamestart", async() => {
+    gameStart= async(io, socket,roomList) => {
+        socket.on("gameStart", async() => {
             try{
                 let userList=roomList[socket.index].userList;
                 let owner=userList.find(ele=>ele.userId===roomList[socket.index].ownerId);
                 let guest=userList.find(ele=>ele.userId!==roomList[socket.index].ownerId);
-                let createInfo=await gameService.createGame(socket.room,owner,guest,"owner");
-                io.to(socket.room).emit("game",{create:createInfo})
+                await gameService.createGame(socket.room,owner,guest,"owner");
+                io.to(socket.room).emit("gameStart",{turn:[owner.userId,guest.userId]})
             }catch(err){
                 error(err,socket)
             }
         });
     }
-    TurnEnd=async(io,socket)=>{
+    turnEnd=async(io,socket)=>{
         socket.on("turnEnd", async(data) => {
             try{
                 let owner=data.turnEnd.owner
@@ -36,6 +36,17 @@ class Game{
         });
     }
     
+    turnResult=async(io,socket)=>{
+        socket.on("turnResult", async(data) => {
+            
+        })
+    }
+
+    gameEnd=async(io,socket)=>{
+        socket.on("gameEnd", async(data) => {
+            
+        })
+    }
 
 }
 module.exports = Game
