c011d94a6b21ce181d73970a0618fc09f79d3c8d
cesdea <aprkvnem@gmail.com>
2022-09-07 20:50:52 +0900

    add gameStart, 조건 추가 이미 있는경우

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const IO = require("./socket/socket");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger-output");
 
-sequelize
-  .sync({ force: false })
-  .then(() => {
-    console.log("데이터베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// sequelize
+//   .sync({ force: false })
+//   .then(() => {
+//     console.log("데이터베이스 연결 성공");
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 class App {
   app = express();
