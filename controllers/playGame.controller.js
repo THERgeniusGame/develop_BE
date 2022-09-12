0426695fc1a58a48efb8e7ae96e53116ea332e6d
Jiwonemil <jjiwon24@gmail.com>
2022-09-12 22:10:14 +0900

    add game password check api 추가

--- controllers/playGame.controller.js
+++ controllers/playGame.controller.js
@@ -1,12 +1,25 @@
-const Service=require("../services/playGame.service")
+const Service = require("../services/playGame.service");
 
-class PlayGameController{
-    service=new Service();
-    visitGame=async(req,res,next)=>{
-        const {roomId}=req.params
-        const info=await this.service.getRoomInfo(roomId);
-        return res.json({info});
+class PlayGameController {
+  service = new Service();
+  visitGame = async (req, res, next) => {
+    const { roomId } = req.params;
+    const info = await this.service.getRoomInfo(roomId);
+    return res.json({ info });
+  };
+  checkPw = async (req, res, next) => {
+    try {
+      const { roomId } = req.params;
+      const { password } = req.body;
+      if (password === undefined) {
+        res.json({ message: "Invalid-password" });
+      }
+      const checkPw = await this.service.checkPw(roomId, password);
+      res.json({ checkPw });
+    } catch (error) {
+      next(error);
     }
+  };
 }
 
-module.exports=PlayGameController
+module.exports = PlayGameController;
--- routes/playGame.route.js
+++ routes/playGame.route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const gameController=require("../controllers/playGame.controller")
-const GameController=new gameController();
-router.get('/testCode/:roomId', function(req, res) {
-    res.sendFile(__dirname + '/static/index.html');
+const gameController = require("../controllers/playGame.controller");
+const GameController = new gameController();
+router.get("/testCode/:roomId", function (req, res) {
+  res.sendFile(__dirname + "/static/index.html");
 });
-router.get('/testCode2/:roomId', function(req, res) {
-    res.sendFile(__dirname + '/static/index3.html');
+router.get("/testCode2/:roomId", function (req, res) {
+  res.sendFile(__dirname + "/static/index3.html");
 });
-router.get('/:roomId', GameController.visitGame);
+router.get("/:roomId", GameController.visitGame);
+//상세방입장 비번 확인
+router.post("/:roomId", GameController.checkPw);
 
-module.exports=router;
+module.exports = router;
