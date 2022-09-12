99fe5784689be75eb02f1c50ccb34a114d2bda7c
Jiwonemil <jjiwon24@gmail.com>
2022-09-12 22:28:57 +0900

    update 상세 게임방 비번 확인 error 수정

--- controllers/playGame.controller.js
+++ controllers/playGame.controller.js
@@ -11,10 +11,11 @@ class PlayGameController {
     try {
       const { roomId } = req.params;
       const { password } = req.body;
-      if (password === undefined) {
-        res.json({ message: "Invalid-password" });
+      if (!password) {
+        return res.json({ message: "Invalid-password" });
       }
       const checkPw = await this.service.checkPw(roomId, password);
+      console.log(checkPw);
       res.json({ checkPw });
     } catch (error) {
       next(error);
