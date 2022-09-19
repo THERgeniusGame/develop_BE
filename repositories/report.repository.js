46160acc3e1982516fdb891cfd0d12f2d51b10ec
cesdea <aprkvnem@gmail.com>
2022-09-19 19:52:53 +0900

    create report.repo,getOne,All,create,delete,update

---
+++ repositories/report.repository.js
@@ -0,0 +1,102 @@
+const { Reports, Users } = require("../models");
+const sequelize = require("sequelize");
+const Op = sequelize.Op;
+require("dotenv").config();
+const env = process.env;
+
+module.exports = class ReportRepository {
+    //전체 신고 목록 
+    findAllBugReport = async ()=>{
+        const list = await Reports.findAll({
+            raw: true,
+            where:{
+                reportCategory:0
+            }
+        })
+        return  list.length;
+    }
+
+    //offset페이지 신고 목록
+    findAllBugReportPage = async (offset) => {
+        const list = await Reports.findAll({
+          offset: offset,
+          limit: env.REPORT_PAGE_COUNT/1,
+          raw: true,
+          attributes: [
+            "reportId",
+            "reportTitle",
+            "createdAt"
+          ],
+          where:{
+            reportCategory:0
+          },
+          include: [
+            {
+              model: Users,
+              attributes: ["nickname"],
+            },
+          ],
+        });
+        return list;
+    }
+
+    //신고상세
+    findOneBugReport = async (reportId) => {
+        const report=await Reports.findOne({
+            attributes:[
+                "reportId",
+                "reportTitle",
+                "reportContent",
+                "createdAt",
+            ],
+            raw:true,
+            where:{
+                reportId
+            },
+            include: [
+                {
+                  model: Users,
+                  attributes: ["nickname"],
+                },
+            ],
+        })
+        return report
+    }
+
+    //신고 작성
+    createBugReport = async (userId,reportTitle,reportContent) => {
+        const createReport=await Reports.create({
+            userId,
+            reportTitle,
+            reportContent,
+            reportCategory:0
+        })
+        return createReport
+    }
+
+    //신고수정
+    updateReport = async (reportId,reportTitle,reportContent) => {
+        const update=await Reports.update(
+            {
+                reportTitle,
+                reportContent
+            },
+            {
+                where:{
+                    reportId,
+                }
+            }
+        )
+        return update;
+    }
+
+    //신고 삭제
+    deleteReport = async (reportId) => {
+        const deleteReport=await Reports.destroy({
+            where: {
+                reportId
+            },
+        });
+        return deleteReport;
+    }
+}
