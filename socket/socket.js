fee13f8f2270c949acee27591036149e8add3a71
cesdea <aprkvnem@gmail.com>
2022-09-21 16:26:54 +0900

    add switch https env

--- socket/socket.js
+++ socket/socket.js
@@ -1,52 +1,60 @@
 const https = require("https");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const { Server } = require("socket.io");
-const EventConnection=require("./event/connection")
+const EventConnection = require("./event/connection");
+require("dotenv").config();
+const env = process.env;
+
 class IO {
-  eventConnection=new EventConnection
+  eventConnection = new EventConnection();
   constructor(app) {
-    try{
-      this.server = https.createServer({
-        ca: fs.readFileSync(
-          "/etc/letsencrypt/live/sparta-emil.shop/fullchain.pem"
-        ),
-        key: fs
-          .readFileSync(
-            path.resolve(
-              process.cwd(),
-              "/etc/letsencrypt/live/sparta-emil.shop/privkey.pem"
+    try {
+      if (env.CHECK_HTTPS==1) {
+        this.server = https.createServer(
+          {
+            ca: fs.readFileSync(
+              "/etc/letsencrypt/live/sparta-emil.shop/fullchain.pem"
             ),
-            "utf8"
-          )
-          .toString(),
-        cert: fs
-          .readFileSync(
-            path.resolve(
-              process.cwd(),
-              "/etc/letsencrypt/live/sparta-emil.shop/cert.pem"
-            ),
-            "utf8"
-          )
-          .toString(),
-      },app);
+            key: fs.readFileSync(
+                path.resolve(
+                  process.cwd(),
+                  "/etc/letsencrypt/live/sparta-emil.shop/privkey.pem"
+                ),
+                "utf8"
+              )
+              .toString(),
+            cert: fs.readFileSync(
+                path.resolve(
+                  process.cwd(),
+                  "/etc/letsencrypt/live/sparta-emil.shop/cert.pem"
+                ),
+                "utf8"
+              )
+              .toString(),
+          },
+          app
+        );
+      } else {
+        this.server = http.createServer(app);
+      }
       this.connectServer(this.server);
-    }catch(err){
-      console.log(err)
+    } catch (err) {
+      console.log(err);
     }
   }
-  
+
   connectServer = async (server) => {
-    try{
+    try {
       let io = new Server(server, {
         cors: {
           origin: "*",
           methods: ["GET", "POST"],
-        }
+        },
       });
-      this.eventConnection.connection(io)
-    }catch(err){
-    }
+      this.eventConnection.connection(io);
+    } catch (err) {}
   };
 }
 
