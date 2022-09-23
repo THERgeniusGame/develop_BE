d17e2500ffa226c3df4742c5da06ddc67fc912cd
Juseok <108967786+Jugeok@users.noreply.github.com>
2022-09-23 15:37:42 +0900

    update nodemailer logic

--- template/auth.js
+++ template/auth.js
@@ -3,19 +3,21 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 const path = require('path');
+const { Emails } = require("../models");
 const { nextTick } = require('process');
 var appDir = path.dirname(require.main.filename);
 
 
-router.post('/', async(req, res) => {
-    let authNum = Math.random().toString().substr(2,6);
+router.post('/', async(req, res, next) => {
+    const email = req.body;
+    const authNum = Math.random().toString().substr(2,6);
     let emailTemplete;
     ejs.renderFile(appDir+'/template/authMail.ejs', {authCode : authNum}, function (err, data) {
       if(err){console.log(err)}
       emailTemplete = data;
     });
     try {
-    let transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         service: 'gmail',
         host: 'smtp.gmail.com',
         port: 587,
@@ -26,7 +28,7 @@ router.post('/', async(req, res) => {
         },
     });
 
-    let mailOptions = {
+    const mailOptions = {
         from: `덜지니어스`,
         to: req.body.email,
         subject: '덜지니어스 회원가입 인증번호입니다.',
@@ -42,6 +44,14 @@ router.post('/', async(req, res) => {
         res.send(authNum);
         transporter.close()
     });
+    
+    const confirmEamil = await Emails.findOne({where:{email}})
+    // console.log(email)
+    // if(confirmEamil){
+    //     await Emails.update({code:authcode},{where:email})
+    // } else{
+    //     await Emails.create({email,code:authcode})
+    // }
 } catch(err) {
     next(err);
 }
