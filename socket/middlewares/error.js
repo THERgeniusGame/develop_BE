8075026a739310d2257f35b1e4514c8ce4f17529
cesdea <aprkvnem@gmail.com>
2022-09-27 21:21:19 +0900

    update error handling

--- socket/middlewares/error.js
+++ socket/middlewares/error.js
@@ -1,6 +1,10 @@
 const error =(err,io,socket) => {
   console.log(err)
-  io.to(socket.id).emit("error",{error:err.message})
+  io.to(socket.id).emit("error",{error:err.message}).then((success)=>{
+    if(success){
+      console.log("error catch")
+    }
+  })
   if(err.message==="Wrong-Url"){
     socket.disconnect();
   }
