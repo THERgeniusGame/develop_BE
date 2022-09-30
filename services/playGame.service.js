0c4fc6141daf953c60867801b558e30da9d8f596
cesdea <aprkvnem@gmail.com>
2022-09-30 17:14:15 +0900

    fix error winCount inf

--- services/playGame.service.js
+++ services/playGame.service.js
@@ -98,9 +98,7 @@ class Service {
       throw err;
     }
   };
-
-  EndGame = async (p1, p2) => {
-    let result = this.game.endGame(p1, p2);
+  EndGameWinLose=async(p1,p2)=>{
     let total1 = await this.userService.upTotal(p1.userId);
     let total2 = await this.userService.upTotal(p2.userId);
     if (result.winner === p1.nickname) {
@@ -110,6 +108,10 @@ class Service {
       const win2 = await this.userService.upWin(p2.userId);
       const lose1 = await this.userService.uplose(p1.userId);
     }
+  }
+
+  EndGame = async (p1, p2) => {
+    let result = this.game.endGame(p1, p2);
     // await this.userRepo.
     return result;
   };
